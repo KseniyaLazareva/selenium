17c337bd2b48d44be0c699cd13a00f1049747dd1
neto study <kseniyalazare@yandex.ru>
2023-05-04 17:17:14 +0300

    add test commit

--- src/test/java/CreditCardTest.java
+++ src/test/java/CreditCardTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.List;
 
